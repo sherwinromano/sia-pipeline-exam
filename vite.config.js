5d579b45bb647696755d47b00afbda52512d39f7
sherwinromano <sherwinromano@users.noreply.github.com>
2025-01-10 07:04:00 +0000

    deploy: c71e7ad2233b19e8cbc5d604666efd69fdf0550a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sia-pipeline-exam/",
+  base: "/sia-pipeline-exam/login",
   plugins: [react()],
 });
